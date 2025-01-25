8e8997d7be4eab967c48ce61e93c862705867c01
Ester Carnassier <imwonderchild@gmail.com>
2025-01-25 14:02:17 +0100

    initial commit

---
+++ src/main/java/cz/ctu/fee/dsv/semwork/model/Node.java
@@ -0,0 +1,27 @@
+package cz.ctu.fee.dsv.semwork.model;
+
+public class Node {
+    private final String nodeId;
+    private final RabbitMQService rabbitMQService;
+
+    public Node(String nodeId, RabbitMQService rabbitMQService) {
+        this.nodeId = nodeId;
+        this.rabbitMQService = rabbitMQService;
+    }
+
+    public void start() throws Exception {
+        rabbitMQService.getChannel().queueBind("updates_queue", "updates_exchange", "");
+
+        // Подписываемся на обменник
+        rabbitMQService.getChannel().basicConsume("updates_queue", true, (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody());
+            System.out.println("Node " + nodeId + " received: " + message);
+            // Логика обработки ответа (e.g., GRANT/DENY)
+        }, consumerTag -> {});
+
+        // Отправляем запрос
+        String request = "REQUEST|" + nodeId + "|R1";
+        rabbitMQService.getChannel().basicPublish("", "requests_queue", null, request.getBytes());
+        System.out.println("Node " + nodeId + " sent: " + request);
+    }
+}
